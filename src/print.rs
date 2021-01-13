c3ac8a137fd622d05e785bdd47a0406007b7579b
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-13 16:59:03 +0630

    Print Topic Done

--- src/print.rs
+++ src/print.rs
@@ -3,4 +3,31 @@ pub fn run(){
 // Print Line
 println!("I' newbie in Rust.");
 
+// Basic Formatting 
+println!("{}",1); // String literal {} need
+
+// Arguments
+
+println!("{} is a student.","Aung Myat Moe");
+
+// Arguments Passing
+println!("{} is a {}.","Aung Myat Moe","Web Dev");
+
+//Posistional Arguments
+println!("{0} is a {1} and {0} love {2}.","Aung Myat Moe","Web Dev","Programming and Physics");
+
+// Mamed arguments
+println!("My name is {name} and age is {age}.",name="Aung Myat Moe",age=17);
+
+// Tuple or Place Holder Trait
+println!("Binary Type : {:b} , Hex Type : {:x} , Ocatal Type : {:o} .",10,10,10);
+
+// Debug
+
+println!("{:?}",(1,"Hello World!",true));
+
+//Basic Math
+
+println!("10 + 10 = {}",10+10);
+
 }
