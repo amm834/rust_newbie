9368fe46c99485582e462a6a4719bc468ec87542
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-13 18:15:45 +0630

    Vars Topic Done

---
+++ src/vars.rs
@@ -0,0 +1,21 @@
+pub fn run() {
+
+ // Variables are immutble like const in JS also define() in PHP
+
+ let name = "Aung Myat Moe";
+ let mut age = 16;
+ println!("{} is {} years old in 2019.", name, age);
+ 
+ age = 17;
+ println!("{} is {} years old in 2020.", name, age);
+ 
+ //Constant
+ const INT: i32 = 1;
+ println!("Int : {}",INT);
+ 
+ // Assign Multiple Variables
+ let (my_name,my_age) = ("Aung Myat Moe",18);
+ println!("{} is {} years old in 2020.", my_name,my_age);
+
+
+}
